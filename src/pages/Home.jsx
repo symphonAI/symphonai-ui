2188b44209aa326a8c8e1e3bba830407f1ab2be7
Daniel Gooden <dan.gooden.dev@gmail.com>
2023-05-17 20:02:23 +1000

    Front page CTA completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,17 @@ import React from "react";
 export default function Home() {
   return (
     <div className=" flex flex-row">
-      <div >
-        <h1 className=" font-machina">Introducing symphonAI</h1>
-        <p>
+      <div className=" mt-44 ml-6 flex-1 flex flex-col gap-6 ">
+        <h1 className=" text-6xl font-machina">Introducing symphonAI</h1>
+        <p className=" text-2xl">
           We&apos;re leveraging power of AI to create tailored playlists that
-          resonate with your emotions and preferences.
+          resonate with your emotions and preferences. Our web app takes your
+          musical prompts and combines them with your listening preferences to
+          create a playlist for you.
         </p>
+        <a className=" w-fit px-4 py-1 border-white border" href="./login">Try <span className=" font-machina">symphonAI</span></a>
       </div>
-      <div>Insert Art here</div>
+      {/* <div className=" mr-6 flex-1">Insert Art here</div> */}
     </div>
   );
 }
